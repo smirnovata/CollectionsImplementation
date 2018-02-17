09f5d3c0f9b0ee9fd8b2bc1a8467a4ffe4034934
TanyaSm <pro100tah9i2009@rambler.ru>
2018-02-17 14:30:02 +0300

    Ererything changes

--- src/Main.java
+++ src/Main.java
@@ -9,42 +9,74 @@ public class Main {
         MyValue one = new MyValue("Tata", "Smit");
 
 
-        MyMap<String, MyValue> myMap1 = new MyMap<>();
-        System.out.println(myMap1.put("1", one));
-        MyValue value = myMap1.get("1");
-        System.out.println(value.name + " " + value.secondname);
-
-
-        //System.out.println(myMap.size());
-        //System.out.println(myMap.isEmpty());
-
-        //System.out.println(myMap.containsKey(7));
-        //System.out.println(myMap.containsKey(0));
-
-        //System.out.println(myMap.containsValue(one));
-
-        System.out.println(myMap.put(new MyKey(7), one));
-        value = myMap.get(new MyKey(7));
-        System.out.println(value.name + " " + value.secondname);
-
-        System.out.println(myMap.put(new MyKey(7), one));
-        value = myMap.get(new MyKey(7));
-        System.out.println(value.name + " " + value.secondname);
-
-
-        //System.out.println(myMap.get(new MyKey(7)));
-        //System.out.println(value.name + " " + value.secondname);
-        System.out.println(myMap.get(new MyKey(6)));
+        //MyMap<String, MyValue> myMap1 = new MyMap<>();
+        //System.out.println(myMap1.put("1", one));
+        //MyValue value = myMap1.get("1");
         //System.out.println(value.name + " " + value.secondname);
 
 
-        //System.out.println(myMap.size());
-        //System.out.println(myMap.isEmpty());
+        System.out.println("size = " + myMap.size());
+        System.out.println("Empty? " + myMap.isEmpty());
 
-        //System.out.println(myMap.containsKey(7));
-        //System.out.println(myMap.containsKey(6));
+        System.out.println("Contains1 (false) " + myMap.containsKey(new MyKey(7)));
+        System.out.println("Contains2 (false) " + myMap.containsKey(new MyKey(0)));
+        System.out.println("Contains3 (false) " + myMap.containsValue(one));
 
-        //System.out.println(myMap.containsValue(one));
+        System.out.println("Get " + myMap.get(new MyKey(3)));
+
+        myMap.put(new MyKey(7), one);
+
+        MyValue value = myMap.get(new MyKey(7));
+        System.out.println(value.name + " " + value.secondname);
+
+        System.out.println("Contains4 (true) " + myMap.containsKey(new MyKey(7)));
+        System.out.println("Contains5 (false) " + myMap.containsKey(new MyKey(0)));
+        System.out.println("Contains6 (true) " + myMap.containsValue(one));
+        System.out.println("Contains7 (false) " + myMap.containsValue(new MyValue("T", "S")));
+
+        System.out.println("size = " + myMap.size());
+        System.out.println("Empty? " + myMap.isEmpty());
+
+        myMap.put(new MyKey(6), new MyValue("S", "D"));
+
+        value = myMap.get(new MyKey(6));
+        System.out.println(value.name + " " + value.secondname);
+
+        System.out.println("Contains8 (true) " + myMap.containsKey(new MyKey(7)));
+        System.out.println("Contains9 (true) " + myMap.containsKey(new MyKey(6)));
+        System.out.println("Contains10 (false) " + myMap.containsKey(new MyKey(3)));
+        System.out.println("Contains11 (true) " + myMap.containsValue(one));
+        System.out.println("Contains12 (true) " + myMap.containsValue(new MyValue("S", "D")));
+        System.out.println("Contains13 (false) " + myMap.containsValue(new MyValue("T", "S")));
+
+        System.out.println("size = " + myMap.size());
+
+
+        myMap.put(new MyKey(7), new MyValue("T", "S"));
+
+        value = myMap.get(new MyKey(7));
+        System.out.println(value.name + " " + value.secondname);
+
+        System.out.println("Contains14 (true) " + myMap.containsKey(new MyKey(7)));
+        System.out.println("Contains15 (true) " + myMap.containsKey(new MyKey(6)));
+        System.out.println("Contains16 (false) " + myMap.containsKey(new MyKey(3)));
+        System.out.println("Contains17 (false) " + myMap.containsValue(one));
+        System.out.println("Contains18 (true) " + myMap.containsValue(new MyValue("S", "D")));
+        System.out.println("Contains19 (true) " + myMap.containsValue(new MyValue("T", "S")));
+
+        myMap.put(new MyKey(3), new MyValue("Se", "Da"));
+        myMap.put(new MyKey(4), new MyValue("Ta", "Smi"));
+
+        System.out.println("size = " + myMap.size());
+
+        myMap.remove(new MyKey(3));
+
+        System.out.println("size = " + myMap.size());
+
+
+//        System.out.println("size = " + myMap.size());
+
+
     }
 
     static class MyValue {
@@ -54,6 +86,21 @@ public class Main {
         public MyValue(String name, String secondname) {
             this.name = name;
             this.secondname = secondname;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            MyValue myValue = (MyValue) o;
+            return Objects.equals(name, myValue.name) &&
+                    Objects.equals(secondname, myValue.secondname);
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(name, secondname);
         }
     }
 
--- src/MyMap.java
+++ src/MyMap.java
@@ -18,13 +18,26 @@ public class MyMap<K, V> implements Map<K, V> {
 
     @Override
     public boolean containsKey(Object key) {
-        // проверка  наличия конкретнного элемента по key
+        for (MapContent content : linkedList) {
+            if (content.key.hashCode() == key.hashCode()) {
+                if (content.key.equals(key)) {
+                    return true;
+                }
+            }
+        }
         return false;
-}
+    }
 
     @Override
     public boolean containsValue(Object value) {
-        // проверка  наличия конкретнного элемента по key
+        for (MapContent content : linkedList) {
+            if (content.value.hashCode() == value.hashCode()) {
+                if (content.value.equals(value)) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
@@ -44,6 +57,18 @@ public class MyMap<K, V> implements Map<K, V> {
 
     @Override
     public V put(K key, V value) {
+        if (linkedList.size() != 0) {
+            for (MapContent content : linkedList) {
+                if (content.key.hashCode() == key.hashCode()) {
+                    if (content.key.equals(key)) {
+                        content.key = key;
+                        content.value = value;
+                        return null;
+                    }
+                }
+            }
+        }
+
         MapContent mapContent = new MapContent(key, value);
         linkedList.add(mapContent);
         return null;
@@ -51,56 +76,66 @@ public class MyMap<K, V> implements Map<K, V> {
 
     @Override
     public V remove(Object key) {
+        for (MapContent content : linkedList) {
+            if (key.hashCode() == content.key.hashCode()) {
+                if (content.key.equals(key)) {
+                    linkedList.remove(content);
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
-
+        //void putAll(Map<? extends K, ? extends V> map): добавляет в коллекцию все объекты из отображения map
     }
 
     @Override
     public void clear() {
-
+        //void clear(): очищает коллекцию
     }
 
     @Override
     public Set<K> keySet() {
         return null;
+        //Set<K> keySet(): возвращает набор всех ключей отображения
     }
 
     @Override
     public Collection<V> values() {
+        //Collection<V> values(): возвращает набор всех значений отображения
         return null;
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
         return null;
+        //Set<Map.Entry<K, V>> entrySet(): возвращает набор элементов коллекции. Все элементы представляют объект Map.Entry
     }
 
-private class MapContent {
-    K key;
-    V value;
+    private class MapContent {
+        K key;
+        V value;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MapContent that = (MapContent) o;
-        return Objects.equals(key, that.key) &&
-                Objects.equals(value, that.value);
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            MapContent that = (MapContent) o;
+            return Objects.equals(key, that.key) &&
+                    Objects.equals(value, that.value);
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(key, value);
+        }
+
+        public MapContent(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(key, value);
-    }
-
-    public MapContent(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-}
 }
